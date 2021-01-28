d6a954b14e8700a7b80b74d84a1181aa91c6a49a
Chase <chase@chaseburr.com>
2021-01-28 14:57:58 -0700

    added multiple rows to home page ;

--- src/App.js
+++ src/App.js
@@ -1,7 +1,27 @@
 import "./App.css";
 
+// Component imports
+import { Row } from "./components/exports";
+
+// API
+import requests from "./requests";
+
 function App() {
-   return <div className="App"></div>;
+   return (
+      <div className="App">
+         <Row
+            isLarge
+            header="HULU ORIGINALS"
+            fetchUrl={requests.fetchHuluOriginals}
+         />
+         <Row header="MOVIES FOR YOU" fetchUrl={requests.fetchComedyMovies} />
+         <Row
+            isLarge
+            header="FX ON HULU"
+            fetchUrl={requests.fetchComedyMovies}
+         />
+      </div>
+   );
 }
 
 export default App;
