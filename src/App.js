bf5d462b5a1477ccdf2c3222af8f0420526a7571
Chase <chase@chaseburr.com>
2021-01-28 17:00:01 -0700

    updated hhome page

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import "./App.css";
 
 // Component imports
-import { Row } from "./components/exports";
+import { Row, Header, RowLong } from "./components/exports";
 
 // API
 import requests from "./requests";
@@ -9,17 +9,24 @@ import requests from "./requests";
 function App() {
    return (
       <div className="App">
-         <Row
-            isLarge
-            header="HULU ORIGINALS"
-            fetchUrl={requests.fetchHuluOriginals}
-         />
-         <Row header="MOVIES FOR YOU" fetchUrl={requests.fetchComedyMovies} />
-         <Row
-            isLarge
-            header="FX ON HULU"
-            fetchUrl={requests.fetchComedyMovies}
-         />
+         <Header fetchUrl={requests.fetchHuluOriginals} />
+         <div className="container">
+            <Row
+               isLarge
+               header="HULU ORIGINALS"
+               fetchUrl={requests.fetchHuluOriginals}
+            />
+            <Row
+               header="MOVIES FOR YOU"
+               fetchUrl={requests.fetchComedyMovies}
+            />
+            <Row
+               isLarge
+               header="FX ON HULU"
+               fetchUrl={requests.fetchHorrorMovies}
+            />
+            <RowLong fetchUrl={requests.fetchActionMovies} />
+         </div>
       </div>
    );
 }
