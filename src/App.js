fa8cb24a7d78ee435a7980421159e3bf325a9da4
Chase <chase@chaseburr.com>
2021-01-29 11:11:32 -0700

    removed requests input

--- src/App.js
+++ src/App.js
@@ -1,33 +1,23 @@
 import "./App.css";
 
 // Component imports
-import { Row, Header, RowLong, Navbar } from "./components/exports";
+import { Navbar } from "./components/exports";
+import { HashRouter as Router, Route, Switch } from "react-router-dom";
 
-// API
-import requests from "./requests";
+// Pages
+import { Home, TV } from "./pages/exports";
 
+// App Component
 function App() {
    return (
       <div className="App">
-         <Navbar />
-         <Header fetchUrl={requests.fetchHuluOriginals} />
-         <div className="container">
-            <Row
-               isLarge
-               header="HULU ORIGINALS"
-               fetchUrl={requests.fetchHuluOriginals}
-            />
-            <Row
-               header="MOVIES FOR YOU"
-               fetchUrl={requests.fetchComedyMovies}
-            />
-            <Row
-               isLarge
-               header="FX ON HULU"
-               fetchUrl={requests.fetchHorrorMovies}
-            />
-            <RowLong fetchUrl={requests.fetchActionMovies} />
-         </div>
+         <Router>
+            <Navbar />
+            <Switch>
+               <Route path="/" component={Home} />
+               <Route path="/tv" component={TV} />
+            </Switch>
+         </Router>
       </div>
    );
 }
