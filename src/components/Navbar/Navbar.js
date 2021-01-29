cae5f607249fa1419bce09cb865769a7084b287d
Chase <chase@chaseburr.com>
2021-01-28 18:41:13 -0700

    added Navbar component

---
+++ src/components/Navbar/Navbar.js
@@ -0,0 +1,34 @@
+import React, { useState } from "react";
+import "./Navbar.css";
+
+function Navbar() {
+   const [scroll, setScroll] = useState(false);
+
+   window.addEventListener("scroll", () => {
+      if (window.pageYOffset > 10) {
+         setScroll(true);
+      } else setScroll(false);
+   });
+   return (
+      <nav className={scroll && "navbar__filled"}>
+         <img
+            className="nav__logo"
+            src="https://cdn.worldvectorlogo.com/logos/hulu.svg"
+            alt=""
+         />
+         <ul className="nav__items">
+            <li className="nav__item">HOME</li>
+            <li className="nav__item">MY STUFF</li>
+            <li className="nav__item">TV SHOWS</li>
+            <li className="nav__item">MOVIES</li>
+            <li className="nav__item">HUBS</li>
+         </ul>
+         <div className="user_items">
+            <i className="fas fa-search"></i>
+            <i className="fas fa-user"></i>
+         </div>
+      </nav>
+   );
+}
+
+export default Navbar;
