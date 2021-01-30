385ee98001de52eaa2ce541151185eb62b990e24
Chase <chase@chaseburr.com>
2021-01-30 12:00:50 -0700

    added RowGenre export

--- src/components/exports.js
+++ src/components/exports.js
@@ -1,4 +1,5 @@
 export { default as Row } from "./Row/Row";
 export { default as RowLong } from "./Row/RowLong";
+export { default as RowGenre } from "./Row/RowGenre";
 export { default as Header } from "./Header/Header";
 export { default as Navbar } from "./Navbar/Navbar";
