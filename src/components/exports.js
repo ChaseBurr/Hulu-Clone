bcf36bd42c0d5987e87d3f0024fc1723e84b723a
Chase <chase@chaseburr.com>
2021-01-30 15:26:59 -0700

    added RowHalf export

--- src/components/exports.js
+++ src/components/exports.js
@@ -1,5 +1,6 @@
 export { default as Row } from "./Row/Row";
 export { default as RowLong } from "./Row/RowLong";
+export { default as RowHalf } from "./Row/RowHalf";
 export { default as RowGenre } from "./Row/RowGenre";
 export { default as Header } from "./Header/Header";
 export { default as Navbar } from "./Navbar/Navbar";
