5c4ae4528689121d0fe2845188ffb3d9c59507bf
Chase <chase@chaseburr.com>
2021-01-30 12:02:00 -0700

    added loading state

--- src/components/Row/Row.jsx
+++ src/components/Row/Row.jsx
@@ -14,6 +14,8 @@ const base_url = "https://image.tmdb.org/t/p/original";
 
 function Row({ header, isLarge, fetchUrl }) {
    const [shows, setShows] = useState([]);
+   const [isLoading, setIsLoading] = useState(false);
+   const [rowId, setRowId] = useState(0);
    const randomRowId = Math.floor(Math.random() * 100);
    let gradientNumber = 0;
 
@@ -21,87 +23,95 @@ function Row({ header, isLarge, fetchUrl }) {
       async function fetchData() {
          const { data } = await axios.get(fetchUrl);
          setShows(data.results);
+         setIsLoading(true);
       }
       fetchData();
    }, [fetchUrl]);
 
    return (
-      <div className={`row`}>
-         <h2 className="row__header">{header}</h2>
-         <div className={`posters row_id_${randomRowId}`}>
-            <button
-               className="button__left"
-               onClick={() => {
-                  document.querySelector(
-                     `.row_id_${randomRowId}`
-                  ).scrollLeft -= 1000;
-               }}
-            >
-               <i className="fas fa-arrow-left"></i>
-            </button>
-            {shows.map((show) => (
-               <>
-                  <div
-                     key={shows.id}
-                     className={`${isLarge ? "poster__large" : "poster"}`}
+      <>
+         {isLoading && (
+            <div className={`row`} key={rowId}>
+               <h2 className="row__header">{header}</h2>
+               <div className={`posters row_id_${randomRowId}`}>
+                  <button
+                     className="button__left"
+                     onClick={() => {
+                        document.querySelector(
+                           `.row_id_${randomRowId}`
+                        ).scrollLeft -= 1000;
+                     }}
                   >
-                     <img
-                        src={`${base_url}${
-                           isLarge ? show.poster_path : show.backdrop_path
-                        }`}
-                        alt={show.name}
-                     />
-                     {isLarge && (
-                        <>
-                           {
-                              (gradientNumber = Math.floor(
-                                 Math.random() * posterGradient.length
-                              ))
-                           }
-                           <div
-                              className="poster__gradient"
-                              style={{
-                                 background: posterGradient[gradientNumber],
-                              }}
-                           ></div>
-                           <div
-                              className="poster__content"
-                              style={{
-                                 background: contentBackground[gradientNumber],
-                              }}
-                           >
-                              <p>Start Watching</p>
-                              <h2>{show?.name || show?.original_title}</h2>
-                              <div
-                                 className="play__button"
-                                 style={{
-                                    background:
-                                       buttonBackground[gradientNumber],
-                                 }}
-                              >
-                                 <button>
-                                    <i className="fas fa-play"></i>
-                                 </button>
-                                 <h4>PLAY</h4>
-                              </div>
-                           </div>
-                        </>
-                     )}
-                  </div>
-               </>
-            ))}
-            <button
-               className="button__right"
-               onClick={() => {
-                  document.querySelector(
-                     `.row_id_${randomRowId}`
-                  ).scrollLeft += 1000;
-               }}
-            >
-               <i className="fas fa-arrow-right"></i>
-            </button>
-         </div>
-      </div>
+                     <i className="fas fa-arrow-left"></i>
+                  </button>
+                  {shows.map((show) => (
+                     <>
+                        <div
+                           className={`${isLarge ? "poster__large" : "poster"}`}
+                        >
+                           <img
+                              src={`${base_url}${
+                                 isLarge ? show.poster_path : show.backdrop_path
+                              }`}
+                              alt={show.name}
+                           />
+                           {isLarge && (
+                              <>
+                                 {
+                                    (gradientNumber = Math.floor(
+                                       Math.random() * posterGradient.length
+                                    ))
+                                 }
+                                 <div
+                                    className="poster__gradient"
+                                    style={{
+                                       background:
+                                          posterGradient[gradientNumber],
+                                    }}
+                                 ></div>
+                                 <div
+                                    className="poster__content"
+                                    style={{
+                                       background:
+                                          contentBackground[gradientNumber],
+                                    }}
+                                 >
+                                    <p>Start Watching</p>
+                                    <h2>
+                                       {show?.name || show?.original_title}
+                                    </h2>
+                                    <div
+                                       className="play__button"
+                                       style={{
+                                          background:
+                                             buttonBackground[gradientNumber],
+                                       }}
+                                    >
+                                       <button>
+                                          <i className="fas fa-play"></i>
+                                       </button>
+                                       <h4>PLAY</h4>
+                                    </div>
+                                 </div>
+                              </>
+                           )}
+                        </div>
+                     </>
+                  ))}
+                  <button
+                     className="button__right"
+                     onClick={() => {
+                        document.querySelector(
+                           `.row_id_${randomRowId}`
+                        ).scrollLeft += 1000;
+                     }}
+                  >
+                     <i className="fas fa-arrow-right"></i>
+                  </button>
+               </div>
+            </div>
+         )}
+      </>
    );
 }
 
