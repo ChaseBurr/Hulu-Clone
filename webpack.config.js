6d09583c6db90763fb5ad6d91b316056d79405c2
Chase <chase@chaseburr.com>
2021-02-04 19:45:34 -0700

    added webpack config files

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+     mode: "development",
+     entry: path.resolve(__dirname, "src/index.js"),
+     performance: {
+          maxAssetSize: 100000,
+     },
+     module: {
+          rules: [
+               {
+                    test: /\.js$/,
+                    exclude: /node_modules/,
+                    use: {
+                         loader: "babel-loader",
+                         options: {
+                              presets: ["@babel/preset-env"],
+                         },
+                    },
+               },
+               {
+                    test: /\.s[ac]ss$/i,
+                    use: [
+                         "style-loader", // inject into DOM
+                         "css-loader", //Turns into commonjs
+                         "sass-loader", // turns sass into css
+                    ],
+               },
+               {
+                    test: /\.css$/i,
+                    use: [
+                         "style-loader", // inject into DOM
+                         "css-loader", //Turns into commonjs
+                    ],
+               },
+          ],
+     },
+     plugins: [
+          new HtmlWebpackPlugin({
+               template: path.resolve(__dirname, "public/index.html"),
+               filename: "index.html",
+          }),
+     ],
+};
